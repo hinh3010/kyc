326f702ff149e9ef88b1d8fee18dfff30caf5fcb
hinhtv <xuanhinh301099@gmail.com>
2023-08-16 21:47:55 +0700

    setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { KycComponent } from './kyc/kyc.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '' ,component: HomeComponent},
+  {path : 'kyc' ,component: KycComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { HomeComponent } from './home/home.component';
+import { KycComponent } from './kyc/kyc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    ContentComponent,
+    HomeComponent,
+    KycComponent
   ],
   imports: [
     BrowserModule,
